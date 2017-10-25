dd1a23c712c99f527a068330e675f4b0342c0e2a
gamemox <32118594+gamemox@users.noreply.github.com>
2017-10-25 00:58:11 -0300

    implementacion return

--- app/Http/Controllers/AbonosController.php
+++ app/Http/Controllers/AbonosController.php
@@ -8,23 +8,22 @@ use App\Abonos;
 class AbonosController extends Controller
 {
       function getAbonos(Request $request,$rut){
+        header("Access-Control-Allow-Origin: *");
+        header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+        header('Access-Control-Allow-Credentials: true');            
         $db = app('db');
-        $results = $db->select("SELECT a.*, c.nombre_documento AS NOMBRE_DOCUMENTO, v.valor AS VALOR
-                                ,DATE_FORMAT(STR_TO_DATE(FECHA_CONTABLE, '%d/%m/%Y'), '%Y')ANO
-                                ,DATE_FORMAT(STR_TO_DATE(FECHA_CONTABLE, '%d/%m/%Y'), '%m')MES
-                                FROM abonosfc a, ct_tipo_documento c, valorutm v
-                                where knumerut=".$rut."
-                                and c.empresa='03'    
-                                and a.formapag=c.tipo_documento  
-                                and v.mes=DATE_FORMAT(STR_TO_DATE(FECHAPAG, '%d/%m/%Y'), '%m')
-                                ORDER BY ptipcred desc, nrocuota desc,DATE_FORMAT(STR_TO_DATE(fechapag, '%d/%m/%Y'), '%Y%m%d') desc
+        $results = $db->select("SELECT A.*, C.NOMBRE_DOCUMENTO, T.DESCCRED AS NOMBRE_CREDITO,0 as DESCUENTO
+                                FROM abonosfc A, ct_tipo_documento C, tipocred T
+                                WHERE KNUMERUT=".$rut."
+                                AND C.TIPO_DOCUMENTO=A.FORMAPAG
+                                AND A.PTIPCRED=T.PTIPCRED
+                                AND T.PTIPCRED=A.PTIPCRED
+                                ORDER BY A.ptipcred desc, A.nrocuota desc,DATE_FORMAT(STR_TO_DATE(fechapag, '%d/%m/%Y'), '%Y%m%d') desc
                                 ");
-        //and v.ano=DATE_FORMAT(STR_TO_DATE(FECHAPAG, '%d/%m/%Y'), '%Y')
+       
         
-        return response()->json([
-            "contador" => count($results),
-            "datos" => $results,
-        ]);
+        return  json_encode($results);
        
    }
    
